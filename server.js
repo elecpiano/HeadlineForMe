0da4ea256b251d636015a28abb4d22e4a9019f86
Jason Lee <elecpiano@gmail.com>
2014-08-18 07:53:54 +0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    //res.end('Hello World\n');
+    res.end('test');
     dogSayHello();
 }).listen(port);
 
