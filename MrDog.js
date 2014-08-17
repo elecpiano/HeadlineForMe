1d1f15d4afaa31e2b3ff404415203b44f5c0d159
elecpiano <elecpiano@gmail.com>
2014-08-18 07:42:59 +0800

    mrdog

---
+++ MrDog.js
@@ -0,0 +1,10 @@
+function MrDog(nickname) {
+
+    this.nickname = nickname;
+
+    this.greet = function () {
+        document.write("Hello, I am " + this.nickname + "<br />")
+    }
+};
+
+module.exports = Hello;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    dogSayHello();
 }).listen(port);
+
+function dogSayHello() {
+    var MrDog = require("./MrDog");
+    var dog = new MrDog("DoggyDoggy");
+    dog.greet();
+}
