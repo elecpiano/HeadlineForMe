c4e4e1ba7969b4475d2335d370b4c244ba45d9c2
elecpiano <elecpiano@gmail.com>
2014-08-19 00:28:41 +0800

    content type

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.writeHead(200, { 'Content-Type': 'html' });
     //res.end('Hello World\n');
     res.end(dogHtml());
 }).listen(port);
