a0d1827815ae6321438fb9589d5f1f797a5359cd
elecpiano <elecpiano@gmail.com>
2014-08-19 00:44:30 +0800

    debug

--- MrDog.js
+++ MrDog.js
@@ -5,14 +5,6 @@ function MrDog(nickname) {
     this.greet = function () {
         return "Hello, I am " + this.nickname;
     }
-
-    this.getHTML = function () {
-        var fs = require("fs");
-        fs.readfileSync("./1.html", "utf-8",
-            function (err, data) {
-                return;
-            });
-    }
 };
 
 module.exports = MrDog;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var port = process.env.PORT || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     //res.end('Hello World\n');
-    res.end(dogHtml());
+    res.end(getHtml());
 }).listen(port);
 
 function dogGreet() {
@@ -12,8 +12,7 @@ function dogGreet() {
     return dog.greet();
 }
 
-function dogHtml() {
-    var MrDog = require("./MrDog");
-    var dog = new MrDog("Cute Dog");
-    return dog.getHTML();
+function getHtml() {
+    var fs = require("fs");
+    fs.readfileSync("./1.html", "utf-8");
 }
