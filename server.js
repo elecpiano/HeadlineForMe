a7010c66661c849b7575892bc11ee8a59e29debd
Jason Lee <elecpiano@gmail.com>
2014-08-19 17:27:37 +0800

    avos node sdk

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
+
+var AV = require('avoscloud-sdk').AV;
+AV.initialize("3bxcmnvlwxjskd0x71e969hqyect81wiiblw3tcwfhn1otka", "mecdoesr8l8op6anajajykoakd8e2lcw62sh1bs6vcj2ts5x");
+
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     //res.end('Hello World\n');
-    uploadData();
+    
     res.end(getHtml());
 }).listen(port);
 
@@ -14,17 +18,25 @@ function dogGreet() {
 }
 
 function getHtml() {
+    uploadData();
     var fs = require("fs");
     return fs.readFileSync("./1.html", "utf-8");
 }
 
 function uploadData() {
-    var AV = require("./av.js");
-    AV.initialize("3bxcmnvlwxjskd0x71e969hqyect81wiiblw3tcwfhn1otka", "mecdoesr8l8op6anajajykoakd8e2lcw62sh1bs6vcj2ts5x");
-    var TestObject = AV.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({ foo: "bar" }, {
-        success: function (object) {
-        }
-    });
+    var TestObject = AV.Object.extend("TestObject2");
+    var obj = new TestObject();
+obj.set("column1",123);
+obj.set("column2","abc");
+obj.save(null, {
+  success: function(obj) {
+    // Execute any logic that should take place after the object is saved.
+    
+  },
+  error: function(obj, error) {
+    // Execute any logic that should take place if the save fails.
+    // error is a AV.Error with an error code and description.
+    
+  }
+});
 }
